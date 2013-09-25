7c393bce3d76d614b6cf5646c5aeddd61563b389
Andres Keskküla <andres@gitlab.eu>
2013-09-25 11:26:26 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pjax::Application.config.secret_token = '98384fda555375c9c7ca4a161c9be7f2e41636e48bd2952032f4c3703e196bf9532584b9d617966d0d759bd441981ba0b1b089f0671400857082cbed458a6921'
